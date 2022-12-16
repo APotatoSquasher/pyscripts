2e811c19c9cc05b9c002bf39a7575f1a15eba175
APotatoSquasher <120289705+APotatoSquasher@users.noreply.github.com>
2022-12-15 18:08:09 -0600

    Add files via upload

---
+++ start.py
@@ -0,0 +1,23 @@
+
+import random
+
+minnumber = 0
+maxnumber = 500
+secretnumber = random.randint(minnumber,maxnumber)
+question = input(" ")
+while question != secretnumber:
+    
+    question = int(input(f'Choose a number between {minnumber} and {maxnumber} '))
+    
+    
+    try:
+        if secretnumber > int(question):
+            print(f"The number is larger than {question}")
+        elif secretnumber < int(question):
+            print(f"The number is less than {question}")
+        else: 
+            print(f"Congrats! The final number was {secretnumber}!")
+            break
+    except ValueError:
+        print("Please enter an integer")
+
